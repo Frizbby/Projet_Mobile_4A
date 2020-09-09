3b3a0c917f6a4c40e6ce5ddad05056cc2104ed7d
Frizbby <etienne.roche@hotmail.fr>
2020-09-09 10:44:32 +0200

    Add Koin + MVVM Architecture

--- app/src/main/java/com/example/projetmobile4a/MainActivity.kt
+++ app/src/main/java/com/example/projetmobile4a/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.projetmobile4a
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.activity_main.*
+import org.koin.android.ext.android.inject
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val mainViewModel : MainViewModel by inject()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        main_button.setOnClickListener{
+            mainViewModel.onClickedIncrement()
+        }
+
+    mainViewModel.counter.observe(this, Observer{
+        value -> main_text.text = value.toString()
+    })
+
+
     }
+
 }
