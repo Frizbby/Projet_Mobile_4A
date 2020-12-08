1c30de610d41d994aaad1f4f81b25f96261f48a0
Frizbby <etienne.roche@hotmail.fr>
2020-12-08 14:44:08 +0100

    Implémentation des Login Feature

---
+++ app/src/main/java/com/example/projetmobile4a/presentation/main/MainViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.projetmobile4a.presentation.main
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.projetmobile4a.domain.entity.User
+import com.example.projetmobile4a.domain.usecase.CreateUserUseCase
+import com.example.projetmobile4a.domain.usecase.GetUserUseCase
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.Main
+
+class MainViewModel(
+    private val createUserUseCase: CreateUserUseCase,
+    private val getUserUseCase: GetUserUseCase
+) : ViewModel() {
+
+    val loginLiveData : MutableLiveData<LoginStatus> = MutableLiveData()
+
+    fun onClickedLogin(emailUser: String, password: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val user = getUserUseCase.invoke(emailUser)
+            val loginStatus= if(user != null){
+                LoginSuccess(user.email)
+            }
+            else{
+                LoginError
+            }
+
+            withContext(Dispatchers.Main){
+                loginLiveData.value = loginStatus
+            }
+        }
+        //counter.value = (counter.value ?: 0) + 1
+    }
+
+}
